531f7c347acf5617d1115d8450d20e067e6e086d
Denis Vykhodtsev <denis-kadet@mail.ru>
2022-08-30 03:11:37 +0300

    js

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,17 @@
 $(document).ready(function() {
 
+    $('.block-5__slider-wrap').slick({
+        arrows: false,
+        dots: false,
+        arrows: false, // включение стрелок (если не нужны false)
+        asNavFor: '.block-5__imagesnew_dotted', // указываем что навигация для слайдера будет отдельно (указываем класс куда вешаем навигацию),
+        responsive: [{
+            breakpoint: 992,
+            settings: {
+                dots: true,
+            }
+        }, ]
+    });
 
     $('.block-5__slider-wrap').slick({
         arrows: false,
